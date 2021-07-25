1824cc89e10ad8ca9ec5fa043dff3055e7df6c98
Juan Garcia <juan.garcia070699@gmail.com>
2021-07-25 19:49:02 -0300

    Added 5.js

---
+++ ejercicio5.js
@@ -0,0 +1,11 @@
+// Calcular factorial de un numero
+
+const factorial = (n) => {
+  let result = 1;
+  for (let i = 1; i <= n; i++) {
+    result *= i;
+  }
+  return result;
+}
+
+console.log(factorial(4));
